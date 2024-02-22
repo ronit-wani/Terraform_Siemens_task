90eb6f7357b9fa59b7d9368cbe4c46c1d5e5611c
ronit-wani <46595227+ronit-wani@users.noreply.github.com>
2024-02-23 00:06:08 +0100

    Update provider2.tf

--- provider2.tf
+++ provider2.tf
@@ -4,14 +4,15 @@ terraform {
         source = "hashicorp/aws"
         version = "4.67.0"
     }
-    backend "s3" {
-        bucket         = "lb_logs"
-        key            = "global/s3/tf-state/terraform.tfstate"
-        region         = "eu-west-1"
-        dynamodb_table = "dynamodb1"
-        encrypt        = true
-    }
   }
+  backend "s3" {
+      bucket         = "lb_logs"
+      key            = "global/s3/tf-state/terraform.tfstate"
+      region         = "eu-west-1"
+      dynamodb_table = "dynamodb1"
+      encrypt        = true
+  }
+  
 }
 
 
