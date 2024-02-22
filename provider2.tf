4e2ee05d5b2d4fd5533b0db40d8e443eb480c170
ronit-wani <46595227+ronit-wani@users.noreply.github.com>
2024-02-22 23:57:52 +0100

    Update provider2.tf

--- provider2.tf
+++ provider2.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "<ENTER-YOUR-BUCKET-NAME-HERE>"
+    key            = "global/s3/tf-state/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "<ENTER-YOUR-DYNAMODB-NAME-HERE>"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.region
   profile = "default"
